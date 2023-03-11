f6d37ee1b4d91f819133e2de0dfcb9527b27edb7
Vladislav10-11 <vladcerkasin261@gmail.com>
2023-03-11 14:48:34 +0100

    add settings screen UI

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,7 +1,9 @@
+import 'package:budgetapp/constants.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -14,7 +16,127 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Settings')),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        centerTitle: true,
+        toolbarHeight: 80,
+        title: const Text(
+          'Settings',
+          style: TextStyle(color: Colors.black, fontSize: 30),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_new, color: Colors.black),
+          onPressed: () => Get.back(),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                mainColor,
+                secondColor,
+              ]),
+        ),
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.lock),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Pin',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.language),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Language',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.paid),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Currensy',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.exposure_zero),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Round',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.dark_mode),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Theme',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.policy),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'privacy Policy',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.delete_forever),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'delete all data',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
